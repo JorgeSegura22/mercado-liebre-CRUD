09779d5b16781587363eb243ccd970c2a31866bd
Jorge <ramiropipi1815@gmail.com>
2023-12-21 22:23:37 -0300

    Mercado liebre CRUD

---
+++ src/controllers/productsController.js
@@ -0,0 +1,131 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const leerArchivo=require("../data/dbLogica")
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const { v4: uuidv4 } = require('uuid');
+const { param } = require('../app');
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+			res.render('products', { products, toThousand });
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		const id = req.params.id;
+    const product = products.find(product => product.id == id);
+
+    res.render('detail', { product, toThousand,title:"Detalle" });
+	
+	},
+	
+// 
+	
+	// Create - Form to create
+	create: (req, res) => {
+      res.render("product-create-form")
+
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+    let {name,price,discount,category,description,image}=req.body
+
+	let datosDeProducto={
+		id:uuidv4(),
+		name,
+		price,
+		discount,
+		category,
+		description,
+		image
+	}
+	
+	products.push(datosDeProducto)
+
+	fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2), 'utf-8');
+
+ 
+
+
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const  {id}  = req.params
+
+console.log(id)
+let productToEdit=products.find(elemento=>elemento.id== +id)
+
+
+
+		return res.render("product-edit-form",{productToEdit})
+	},
+	// Update - Method to update
+	update: (req, res) => {
+
+		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const { id } = req.params;
+const { name, price, discount, category, description, image } = req.body;
+console.log(id)
+
+const productToUpdate = products.find(elemento => elemento.id === +id);
+
+if (productToUpdate) {
+ 
+  productToUpdate.name = name.trim();
+  productToUpdate.price = +price;
+  productToUpdate.description = description.trim();
+  productToUpdate.discount = +discount;
+  productToUpdate.category = category;
+
+
+  
+  fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2), 'utf-8');
+
+  
+
+ 
+  return res.redirect(`/products/detail/${id}`);
+}
+	},
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+		const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		
+
+		const id = req.params.id;
+		const productToDelete = products.filter(elemento => elemento.id !=+id);
+
+		console.log(productToDelete)
+		
+		 
+		  
+		
+		
+		  
+		  fs.writeFileSync(productsFilePath, JSON.stringify(productToDelete, null, 2), 'utf-8');
+
+		 
+		  res.redirect("/");
+	}
+};
+
+module.exports = controller;
+
